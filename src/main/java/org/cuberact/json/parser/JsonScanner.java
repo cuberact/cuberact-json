65bd98f1e4cebb600d0b518e026aeeafcbd2a433
NkD <michal.nikodim@topmonks.com>
2017-11-23 17:01:42 +0100

    copyright

--- src/main/java/org/cuberact/json/parser/JsonScanner.java
+++ src/main/java/org/cuberact/json/parser/JsonScanner.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Michal Nikodim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.cuberact.json.parser;
 
 import org.cuberact.json.JsonException;
@@ -9,7 +25,7 @@ import static org.cuberact.json.optimize.CharTable.hexBitShift;
 import static org.cuberact.json.optimize.CharTable.toInt;
 
 /**
- * @author Michal Nikodim (michal.nikodim@topmonks.com)
+ * @author Michal Nikodim (michal.nikodim@gmail.com)
  */
 final class JsonScanner {
 
