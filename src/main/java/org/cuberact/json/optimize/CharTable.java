6441b281a340f80d3db312d49027b25ad76fd164
NkD <michal.nikodim@topmonks.com>
2017-12-01 12:04:40 +0100

    constants as long directly

--- src/main/java/org/cuberact/json/optimize/CharTable.java
+++ src/main/java/org/cuberact/json/optimize/CharTable.java
@@ -50,28 +50,28 @@ public final class CharTable {
         CHAR_TO_INT['E'] = 14;
         CHAR_TO_INT['F'] = 15;
 
-        CHAR_TO_LONG['0'] = 0;
-        CHAR_TO_LONG['1'] = 1;
-        CHAR_TO_LONG['2'] = 2;
-        CHAR_TO_LONG['3'] = 3;
-        CHAR_TO_LONG['4'] = 4;
-        CHAR_TO_LONG['5'] = 5;
-        CHAR_TO_LONG['6'] = 6;
-        CHAR_TO_LONG['7'] = 7;
-        CHAR_TO_LONG['8'] = 8;
-        CHAR_TO_LONG['9'] = 9;
-        CHAR_TO_LONG['a'] = 10;
-        CHAR_TO_LONG['b'] = 11;
-        CHAR_TO_LONG['c'] = 12;
-        CHAR_TO_LONG['d'] = 13;
-        CHAR_TO_LONG['e'] = 14;
-        CHAR_TO_LONG['f'] = 15;
-        CHAR_TO_LONG['A'] = 10;
-        CHAR_TO_LONG['B'] = 11;
-        CHAR_TO_LONG['C'] = 12;
-        CHAR_TO_LONG['D'] = 13;
-        CHAR_TO_LONG['E'] = 14;
-        CHAR_TO_LONG['F'] = 15;
+        CHAR_TO_LONG['0'] = 0L;
+        CHAR_TO_LONG['1'] = 1L;
+        CHAR_TO_LONG['2'] = 2L;
+        CHAR_TO_LONG['3'] = 3L;
+        CHAR_TO_LONG['4'] = 4L;
+        CHAR_TO_LONG['5'] = 5L;
+        CHAR_TO_LONG['6'] = 6L;
+        CHAR_TO_LONG['7'] = 7L;
+        CHAR_TO_LONG['8'] = 8L;
+        CHAR_TO_LONG['9'] = 9L;
+        CHAR_TO_LONG['a'] = 10L;
+        CHAR_TO_LONG['b'] = 11L;
+        CHAR_TO_LONG['c'] = 12L;
+        CHAR_TO_LONG['d'] = 13L;
+        CHAR_TO_LONG['e'] = 14L;
+        CHAR_TO_LONG['f'] = 15L;
+        CHAR_TO_LONG['A'] = 10L;
+        CHAR_TO_LONG['B'] = 11L;
+        CHAR_TO_LONG['C'] = 12L;
+        CHAR_TO_LONG['D'] = 13L;
+        CHAR_TO_LONG['E'] = 14L;
+        CHAR_TO_LONG['F'] = 15L;
     }
 
     private CharTable() {
